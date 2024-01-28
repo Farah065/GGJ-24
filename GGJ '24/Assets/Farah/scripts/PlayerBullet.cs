041076badf071b6211ab274ca48fbbc350e63d24
Farah065 <farahahmad065@gmail.com>
2024-01-28 18:03:31 +0200

    enemies

--- GGJ '24/Assets/Farah/scripts/PlayerBullet.cs
+++ GGJ '24/Assets/Farah/scripts/PlayerBullet.cs
@@ -6,6 +6,10 @@ public class PlayerBullet : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+        }
         Destroy(this.gameObject);
     }
 }
--- GGJ '24/Assets/Sharkawy/scripts/meleeEnemyShark.cs
+++ GGJ '24/Assets/Sharkawy/scripts/meleeEnemyShark.cs
@@ -9,19 +9,35 @@ public class meleeEnemyShark : MonoBehaviour
     Rigidbody2D player;
     Vector2 trajectory;
     public float enemySpeed;
+    Animator anim;
+    Transform tr;
     // Start is called before the first frame update
     void Start()
     {
         enemy = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
         trajectory = (player.transform.position - enemy.transform.position).normalized;
+
+        anim = GetComponent<Animator>();
+        tr = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemy.velocity = enemySpeed * trajectory;
-        Vector2 optimal = (player.transform.position - enemy.transform.position).normalized;
-        trajectory = (Vector2.Lerp(trajectory, optimal, 0.2f)).normalized;
+        float d = Mathf.Sqrt(Mathf.Pow(player.position.x - transform.position.x, 2) + Mathf.Pow(player.position.y - transform.position.y, 2));
+        if (d > 3)
+        {
+            enemy.velocity = enemySpeed * trajectory;
+            Vector2 optimal = (player.transform.position - enemy.transform.position).normalized;
+            trajectory = (Vector2.Lerp(trajectory, optimal, 0.2f)).normalized;
+        }
+        else
+        {
+            enemy.velocity = Vector3.zero;
+        }
+
+        float s = Mathf.Sqrt(Mathf.Pow(enemy.velocity.x, 2) + Mathf.Pow(enemy.velocity.y, 2));
+        anim.SetFloat("speed", s);
     }
 }
